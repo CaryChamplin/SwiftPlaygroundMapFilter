4addcc4f3686d7d55479ab67603dadf2d6b942a9
CaryChamplin <cary@champlintechnologiesllc.com>
2021-09-11 20:11:29 -0700

    Added third example to MapFilter where an array's reorder is persisted.

--- MapFilter.playground/Pages/MapFilterExamples.xcplaygroundpage/Contents.swift
+++ MapFilter.playground/Pages/MapFilterExamples.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@
  
  * An array of Garage structs where each Garage consists: id, garageOwner.
  
- * An array of Vehicle structs where each Vehicle consists: id, make, model, year, and garageID.
+ * An array of Vehicle structs where each Vehicle consists: id, make, model, year, garageID, and order.
  
  ### Example 1: Determine which garage owners have the following two cars.
  */
@@ -28,6 +28,40 @@ code(for: "Garages with vehicles:") {
     let garagesWithVehicles = Garage.localGarages.filter( { garageIDs.contains($0.id) } )
     for garage in garagesWithVehicles {
         Special.padPrint(garage.garageOwner)
+    }
+}
+/*:
+ ### Example 3: Persist order of vehicles as modified by a user using SwiftUI List.
+ Re-ordering arrays is one of the cool features about using List in SwiftUI. For this scenario, an array
+ 
+ is retrieved from a database, shown to the user as items in a SwiftUI List, re-ordered by the user, and
+ 
+ then saved back to the DB. Several potential issues may occur in this scenario that include:
+
+ * order may not be correct on next retrieval
+ 
+ * extra items may remain in the DB if any items in the array are deleted
+
+ One method of persisting item order is to capture the updated array index as a parameter in each item when
+ 
+ the array is being saved. For this example, the Vehicle struct's parameter 'order' is used to capture the
+ 
+ updated index order of its array.
+ */
+code(for: "Persist array order:") {
+    print("Sort Vehicle array retrieved from DB by item order")
+    let sortedVehicles = Vehicle.vehicles.sorted { $0.order < $1.order }
+    for vehicle in sortedVehicles {
+        Special.padPrint(vehicle.model, vehicle.order)
+    }
+    print("\nSave index to 'order' for each item in array")
+    let reorderedVehicles = Vehicle.vehicles.enumerated().map { (index, item) -> Vehicle in
+        var localVehicle: Vehicle = item
+        localVehicle.order = index
+        return localVehicle
+    }
+    for vehicle in reorderedVehicles {
+        Special.padPrint(vehicle.model, vehicle.order)
     }
 }
 /*:
--- MapFilter.playground/Sources/Data.swift
+++ MapFilter.playground/Sources/Data.swift
@@ -28,6 +28,7 @@ public struct Vehicle: Codable, Identifiable, Equatable, Hashable {
     public var model:    String
     public var year:     Int
     public var garageID: UUID
+    public var order:    Int
     
     init() {
         self.id         = UUID.init()
@@ -35,23 +36,24 @@ public struct Vehicle: Codable, Identifiable, Equatable, Hashable {
         self.model      = ""
         self.year       = 2021
         self.garageID   = UUID.init()
+        self.order      = 1000
     }
     
-    init(id: UUID, make: String, model: String, year: Int, garageID: UUID) {
+    init(id: UUID, make: String, model: String, year: Int, garageID: UUID, order: Int) {
         self.id         = id
         self.make       = make
         self.model      = model
         self.year       = year
         self.garageID   = garageID
+        self.order      = order
     }
     
     public static var vehicles: [Vehicle] = [
-        Vehicle(id: UUID.init(), make: "Ford",     model: "Shelby Mustang GT350", year: 1965, garageID: UUID(uuidString: "95A0D1D1-491F-46FE-A51B-80FC5B381D76")!),
-        Vehicle(id: UUID.init(), make: "Chevy",    model: "Camaro Z/28",          year: 1967, garageID: UUID(uuidString: "96A5902E-5408-437C-AF44-16F39DF90C5A")!),
-        Vehicle(id: UUID.init(), make: "Chevy",    model: "Corvette L88",         year: 1968, garageID: UUID(uuidString: "95A0D1D1-491F-46FE-A51B-80FC5B381D76")!),
-        Vehicle(id: UUID.init(), make: "Plymouth", model: "Hemi 'Cuda",           year: 1971, garageID: UUID(uuidString: "46F64638-EAD2-49F0-A0A6-C1AD047B225B")!),
-        Vehicle(id: UUID.init(), make: "Pontiac",  model: "GTO",                  year: 1964, garageID: UUID(uuidString: "95A0D1D1-491F-46FE-A51B-80FC5B381D76")!),
-        Vehicle(id: UUID.init(), make: "Ford",     model: "Fairlane Thunderbolt", year: 1964, garageID: UUID(uuidString: "46F64638-EAD2-49F0-A0A6-C1AD047B225B")!)
+        Vehicle(id: UUID.init(), make: "Ford",     model: "Shelby Mustang GT350", year: 1965, garageID: UUID(uuidString: "95A0D1D1-491F-46FE-A51B-80FC5B381D76")!, order: 5),
+        Vehicle(id: UUID.init(), make: "Chevy",    model: "Camaro Z/28",          year: 1967, garageID: UUID(uuidString: "96A5902E-5408-437C-AF44-16F39DF90C5A")!, order: 2),
+        Vehicle(id: UUID.init(), make: "Chevy",    model: "Corvette L88",         year: 1968, garageID: UUID(uuidString: "95A0D1D1-491F-46FE-A51B-80FC5B381D76")!, order: 0),
+        Vehicle(id: UUID.init(), make: "Plymouth", model: "Hemi 'Cuda",           year: 1971, garageID: UUID(uuidString: "46F64638-EAD2-49F0-A0A6-C1AD047B225B")!, order: 4),
+        Vehicle(id: UUID.init(), make: "Pontiac",  model: "GTO",                  year: 1964, garageID: UUID(uuidString: "95A0D1D1-491F-46FE-A51B-80FC5B381D76")!, order: 1),
+        Vehicle(id: UUID.init(), make: "Ford",     model: "Fairlane Thunderbolt", year: 1964, garageID: UUID(uuidString: "46F64638-EAD2-49F0-A0A6-C1AD047B225B")!, order: 3)
     ]
 }
-
